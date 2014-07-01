ef5ec73e2534caf5ad83f543d8a90aae354bd7f7
Steve Schofield <stevebschofield@gmail.com>
2014-07-01 17:21:16 +0100

    complete if, else, ternary

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -13,22 +13,51 @@
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
 
-    /* WORK HERE */
+//    if (dollars == 4) {
+//        itemToReturn = @"get out of my store";
+//    } else if (dollars == 5) {
+//        itemToReturn = @"have some gum";
+//    } else if ( dollars == 6) {
+//        itemToReturn = @"have an apple";
+//    } else if (dollars >= 1000 && dollars < 1000000000) {
+//        itemToReturn = @"have an Apple computer";
+//    } else if (dollars == 1000000000) {
+//        itemToReturn = @"have The Big Apple";
+//    }
+    
+    // I gave it a try with a Case Switch statement for fun....
+    
+    switch (dollars)
+    
+    {
+        case 4:
+            itemToReturn = @"get out of my store";
+            break;
+        case 5:
+            itemToReturn = @"have some gum";
+            break;
+        case 6:
+            itemToReturn = @"have an apple";
+            break;
+        case 1000:
+            itemToReturn = @"have an Apple computer";
+            break;
+        case 1000000000:
+            itemToReturn = @"have The Big Apple";
+            break;
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
-
+    
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    NSUInteger newCost = (self.getsDiscount) ? cost *= 0.75 : cost ;
     
-    return cost;
+    return newCost;
 }
 
 @end
